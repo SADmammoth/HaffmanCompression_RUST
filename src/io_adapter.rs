4aff9f6b7bf8e5f75fe6191ce75741b1968584c0
SADmammoth <lo.ma200018@gmail.com>
2021-07-22 00:51:28 +0300

    refactor: io adapter

--- src/io_adapter.rs
+++ src/io_adapter.rs
@@ -1,24 +1,24 @@
 use std::fs::*;
 use std::io::{self, Result, Write};
-pub fn read() -> String { 
-	println!("Please, enter file path:");
-	let mut path = String::new();
-	io::stdin()
-	.read_line(&mut path)
-	.expect("Failed to read line");
 
-	read_to_string(path.trim())
-	.expect("Something went wrong reading the file")
+pub fn read() -> String {
+    let path = get_path("Please, enter input file path:");
+    read_to_string(path).expect("Something went wrong reading the file")
 }
 
 pub fn write(content: String) -> Result<()> {
-	println!("Please, enter output file path:");
-	let mut path = String::new();
-	io::stdin()
-	.read_line(&mut path)
-	.expect("Failed to read line");
-
-	let mut file = File::create(path.trim())?;
+    let path = get_path("Please, enter output file path:");
+    let mut file = File::create(path)?;
     file.write_all(content.as_bytes())?;
-	Ok(())
+    Ok(())
+}
+
+fn get_path(message: &str) -> String {
+    println!("{}", message);
+    let mut path = String::new();
+    io::stdin()
+        .read_line(&mut path)
+        .expect("Failed to read line");
+
+    path.trim().to_string()
 }
