c2412ec5c5ad13a2f7262fc96f98929c97aba24b
SADmammoth <lo.ma200018@gmail.com>
2021-07-23 00:47:03 +0300

    tests: message compressed; compression consistent

--- src/haffman/compression/mod.rs
+++ src/haffman/compression/mod.rs
@@ -87,3 +87,61 @@ impl<'a> CompressionResult<'a> {
         )
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::haffman::helpers::text_to_bin;
+
+    fn compress<'a>() -> CompressionResult<'a> {
+        // let encoded: &str = "0010001101001010101110000101000111111010000000110001011011000001111100";
+        // let alphabet: Alphabet = Alphabet::new([('r', String::from("0011")), ('e', String::from("0110")), ('i', String::from("0101")), ('m', String::from("0001")), ('f', String::from("100")), ('H', String::from("00100")), ('c', String::from("0111")), (' ', String::from("00101")), ('a', String::from("101")), ('s', String::from("110")), ('p', String::from("0100")), ('o', String::from("111")), ('n', String::from("0000"))].iter().cloned().collect());
+
+        let message = "Haffman compression";
+        let haf_comp = HaffmanCompression::new().compress(message);
+        haf_comp
+    }
+
+    #[test]
+    pub fn message_is_compressed() {
+        let result = compress();
+        // Message is changed
+        assert_ne!(result.message, result.encoded);
+        // Message binary is changed
+        assert_ne!(text_to_bin(result.message), result.get_encoded());
+        // Message size is reduced
+        assert!(text_to_bin(result.message).len() > result.get_encoded().len());
+        // Alphabet size is less than message length
+        assert!(text_to_bin(result.message).len() > result.get_alphabet().len());
+        // Compressed message with alphabet meust be larger than compressed message
+        assert!(result.get_with_injected_alphabet().len() > result.get_encoded().len());
+    }
+
+    #[test]
+    pub fn compression_is_consistent() {
+        let mut result = compress();
+
+        let alphabet = result.get_alphabet().to_string();
+        let compresed = result.get_encoded().to_string();
+        let compressed_with_alphabet = result.get_with_injected_alphabet();
+
+        for _ in 0..1000 {
+            result = compress();
+            assert_eq!(
+                alphabet,
+                result.get_alphabet().to_string(),
+                "Alphabet is not consistent"
+            );
+            assert_eq!(
+                compresed,
+                result.get_encoded().to_string(),
+                "Compressed message is not consistent"
+            );
+            assert_eq!(
+                compressed_with_alphabet,
+                result.get_with_injected_alphabet(),
+                "Compressed message with alphabet is not consistent"
+            );
+        }
+    }
+}
